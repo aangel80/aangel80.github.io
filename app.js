c5131ec1b5a2cf47f2f63d67ec2167e863dd9d4f
aangel80 <angelaroraxo@gmail.com>
2024-09-15 23:10:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener"DOMContentLoaded", () => 
+/*
+    const images = document.querySelectorAll ("img");
+
+    for(const image of images) {
+        fetch ("https://www.disputesregister.org/img/blog/2021/mischondereya.jpg")
+        .then(response => response.json ())
+        .then (data => {
+             image.src = data.message 
+             image.width = 100;
+             image.height = 100;
+            })
+
+    }
+      
+})
